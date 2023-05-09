a372eb5b57681459671ec555aada9d1098999c66
mrbushik <mrbushik1@gmail.com>
2023-05-10 00:02:48 +0200

    spin coin by button

--- src/Components/pages/coinFlip.tsx
+++ src/Components/pages/coinFlip.tsx
@@ -2,28 +2,47 @@ import React, { useEffect, useState } from "react";
 import "../../styles/coinFlip.scss";
 const CoinFlip: React.FC = () => {
   const [coinPosition, setCoinPosition] = useState<string>("");
+  const [randomValue, setRandomValue] = useState<number>(0);
+  let coin: any;
+  useEffect(() => {
+    coin = document.querySelector(".coin");
+  }, []);
 
   useEffect(() => {
-    const coin: any = document.querySelector(".coin");
-    console.log(coin);
+    let timer: any;
 
     if (coin) {
-      coin.classList.remove();
-      coin.style.animation = coinPosition;
-      console.log(`${coinPosition} coin`);
+      coin.removeAttribute("style");
+      if (randomValue) {
+        timer = setTimeout(function () {
+          coin.style.animation = "spin-heads 3s forwards";
+        }, 100);
+      } else {
+        timer = setTimeout(function () {
+          coin.style.animation = "spin-tails 3s forwards";
+        }, 100);
+      }
     }
-  }, [coinPosition]);
+    return () => clearTimeout(timer);
+  }, [randomValue]);
 
   const handleCoinFlip = () => {
-    setCoinPosition("spin-heads 3s forward");
+    handleRandom();
+    coin.style.animation = "3s ease 0s 1 normal forwards running spin-heads";
+    // handleRemoveStyle();
+
+    // setCoinPosition("3s ease 0s 1 normal forwards running spin-heads");
   };
+
+  const handleRandom = () => setRandomValue(Math.floor(Math.random() * 2));
+  const handleRemoveStyle = () => coin.removeAttribute("style");
 
   return (
     <div>
       <div className="container">
-        <div className="coin" id="coin">
+        <div className="coin" id="coin" style={{ animation: coinPosition }}>
           <div className="heads">
-            <img src="https://jkscoinworld.com/wp-content/uploads/2018/05/2013-a-1.jpg"  />
+            <img src="https://jkscoinworld.com/wp-content/uploads/2018/05/2013-a-1.jpg" />
           </div>
           <div className="tails">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKPXdeMWZbX3Vk9Qc3tgGtERTCZNe5z1OyzXN8ZejoIrXgA95Wi4mfTV3BgGr0lGHO5I4&usqp=CAU" />
@@ -35,9 +54,11 @@ const CoinFlip: React.FC = () => {
         </div>
         <div className="buttons">
           <div id="flip-button" onClick={handleCoinFlip}>
-            Flip Coin
+            Bet
           </div>
-          <button id="reset-button">Reset</button>
+          <div id="reset-button" onClick={handleRemoveStyle}>
+            Reset
+          </div>
         </div>
       </div>
     </div>
