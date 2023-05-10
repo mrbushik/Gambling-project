a5d3211a41fc3ec834a0940b50f25f7d40c83a0b
mrbushik <mrbushik1@gmail.com>
2023-05-10 23:36:54 +0200

    fix twice useEffect working and add images

--- src/Components/pages/coinFlip.tsx
+++ src/Components/pages/coinFlip.tsx
@@ -8,31 +8,30 @@ interface coinInfoInterface {
 }
 
 const CoinFlip: React.FC = () => {
+  let initialized: boolean = false;
+  let coin: any;
+
   const [coinInfo, setCoinInfo] = useState<coinInfoInterface>({
     randomValue: 0,
     heads: 0,
     tails: 0,
   });
-  const [randomValue, setRandomValue] = useState<number>(0);
-  let coin: any;
-  useEffect(() => {
-  }, []);
+  useEffect(() => {}, []);
 
   useEffect(() => {
     let timer: any;
-    console.log(coin);
     coin = document.querySelector(".coin");
 
-    if (coin) {
-      console.log("work");
+    if (coin && !initialized) {
+      initialized = true;
       coin.removeAttribute("style");
       if (coinInfo.randomValue < 50000) {
         timer = setTimeout(function () {
           coin.style.animation = "spin-heads 3s forwards";
-          // setCoinInfo((prevState) =>({...prevState, [tails]: prevState.heads++}))
         }, 100);
       } else {
         timer = setTimeout(function () {
+          console.log("tails");
           coin.style.animation = "spin-tails 3s forwards";
         }, 100);
       }
@@ -42,16 +41,12 @@ const CoinFlip: React.FC = () => {
 
   const handleCoinFlip = () => {
     handleRandom();
-    // coin.style.animation = "3s ease 0s 1 normal forwards running spin-heads";
-    // handleRemoveStyle();
-
-    // setCoinPosition("3s ease 0s 1 normal forwards running spin-heads");
   };
 
   const handleRandom = () =>
     setCoinInfo((prevState: coinInfoInterface) => ({
       ...prevState,
-      [randomValue]: Math.floor(Math.random() * 100000),
+      randomValue: Math.floor(Math.random() * 100000),
     }));
   const handleRemoveStyle = () => coin.removeAttribute("style");
 
@@ -60,10 +55,10 @@ const CoinFlip: React.FC = () => {
       <div className="container">
         <div className="coin" id="coin">
           <div className="heads">
-            <img src="https://jkscoinworld.com/wp-content/uploads/2018/05/2013-a-1.jpg" />
+            <img src="https://res.cloudinary.com/drfjcq9hg/image/upload/v1683753396/coin2_zicn4n.png" />
           </div>
           <div className="tails">
-            <img src="ttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKPXdeMWZbX3Vk9Qc3tgGtERTCZNe5z1OyzXN8ZejoIrXgA95Wi4mfTV3BgGr0lGHO5I4&usqp=CAUh" />
+            <img src="https://res.cloudinary.com/drfjcq9hg/image/upload/v1683753396/coin1_ldy6ou.png" />
           </div>
         </div>
         <div className="stats">
