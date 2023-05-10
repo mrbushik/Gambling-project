2ed34313bfdcd9b1ee704f89dfce49c920f7c295
mrbushik <mrbushik1@gmail.com>
2023-05-10 12:36:33 +0200

    edit bug with spin

--- src/Components/pages/coinFlip.tsx
+++ src/Components/pages/coinFlip.tsx
@@ -1,21 +1,35 @@
 import React, { useEffect, useState } from "react";
 import "../../styles/coinFlip.scss";
+
+interface coinInfoInterface {
+  randomValue: number;
+  heads: number;
+  tails: number;
+}
+
 const CoinFlip: React.FC = () => {
-  const [coinPosition, setCoinPosition] = useState<string>("");
+  const [coinInfo, setCoinInfo] = useState<coinInfoInterface>({
+    randomValue: 0,
+    heads: 0,
+    tails: 0,
+  });
   const [randomValue, setRandomValue] = useState<number>(0);
   let coin: any;
   useEffect(() => {
-    coin = document.querySelector(".coin");
   }, []);
 
   useEffect(() => {
     let timer: any;
+    console.log(coin);
+    coin = document.querySelector(".coin");
 
     if (coin) {
+      console.log("work");
       coin.removeAttribute("style");
-      if (randomValue) {
+      if (coinInfo.randomValue < 50000) {
         timer = setTimeout(function () {
           coin.style.animation = "spin-heads 3s forwards";
+          // setCoinInfo((prevState) =>({...prevState, [tails]: prevState.heads++}))
         }, 100);
       } else {
         timer = setTimeout(function () {
@@ -24,28 +38,32 @@ const CoinFlip: React.FC = () => {
       }
     }
     return () => clearTimeout(timer);
-  }, [randomValue]);
+  }, [coinInfo]);
 
   const handleCoinFlip = () => {
     handleRandom();
-    coin.style.animation = "3s ease 0s 1 normal forwards running spin-heads";
+    // coin.style.animation = "3s ease 0s 1 normal forwards running spin-heads";
     // handleRemoveStyle();
 
     // setCoinPosition("3s ease 0s 1 normal forwards running spin-heads");
   };
 
-  const handleRandom = () => setRandomValue(Math.floor(Math.random() * 2));
+  const handleRandom = () =>
+    setCoinInfo((prevState: coinInfoInterface) => ({
+      ...prevState,
+      [randomValue]: Math.floor(Math.random() * 100000),
+    }));
   const handleRemoveStyle = () => coin.removeAttribute("style");
 
   return (
     <div>
       <div className="container">
-        <div className="coin" id="coin" style={{ animation: coinPosition }}>
+        <div className="coin" id="coin">
           <div className="heads">
             <img src="https://jkscoinworld.com/wp-content/uploads/2018/05/2013-a-1.jpg" />
           </div>
           <div className="tails">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKPXdeMWZbX3Vk9Qc3tgGtERTCZNe5z1OyzXN8ZejoIrXgA95Wi4mfTV3BgGr0lGHO5I4&usqp=CAU" />
+            <img src="ttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKPXdeMWZbX3Vk9Qc3tgGtERTCZNe5z1OyzXN8ZejoIrXgA95Wi4mfTV3BgGr0lGHO5I4&usqp=CAUh" />
           </div>
         </div>
         <div className="stats">
